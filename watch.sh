a36759fbd989a88fe6df437c89fcfe034621ef84
Lu Fan <lf@arenko.group>
2019-10-07 17:08:06 +0100

    DIGEST_HEX workaround

--- watch.sh
+++ watch.sh
@@ -2,5 +2,6 @@
 
 # watch the java files and continously deploy the service
 mvn clean install
+export UUID=$(uuidgen)
 skaffold run -p dev
-reflex -r "\.java$" -- bash -c 'mvn install && skaffold run -p dev'
+reflex -r "\.java$" -- bash -c 'export UUID=$(uuidgen) && mvn install && skaffold run -p dev'
